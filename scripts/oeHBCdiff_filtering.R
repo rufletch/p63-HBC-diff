59b46552c76e914ee2a2fc3f2bb3ff791c1f1eb9
Russell <rufletch@berkeley.edu>
2017-04-14 13:02:41 -0700

    Update oeHBCdiff_filtering.R

--- scripts/oeHBCdiff_filtering.R
+++ scripts/oeHBCdiff_filtering.R
@@ -1,3 +1,7 @@
+#title: Filtering Poor Quality Samples
+#author: Diya Das and Russell Fletcher
+#date: January 30th 2017
+
 rm(list=ls()); options(getClass.msg=FALSE)
 library(scone)
 library(Biobase)
